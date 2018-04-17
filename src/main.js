736c6431465a7f322207240924a54ba03c08c244
Matt Castle <mattcc82@gmail.com>
2018-04-17 14:48:19 -0400

    adding explaination of render/createElement

--- src/main.js
+++ src/main.js
@@ -3,5 +3,15 @@ import App from './App.vue'
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
+  /*
+    take the element specified above "#app"
+    and render the .vue template file "./App.vue"
+    the shorthand is:
+    ---
+    render: function (createElement) {
+      return createElement(App);
+    }
+    createElement() describes the node to be created in the virtual DOM
+  */
   render: h => h(App)
 })
